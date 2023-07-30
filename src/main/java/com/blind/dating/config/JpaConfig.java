1fb4e484ff2c5d8819bc6104a246acafe7bcdc44
pyjhoop <pyjhoop@naver.com>
2023-07-30 23:01:07 +0900

    #4 - 웹에 필요한 도메인 구현
    도메인, auditing 기능까지 구현

---
+++ src/main/java/com/blind/dating/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.blind.dating.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+}
---
+++ src/main/java/com/blind/dating/domain/Message.java
@@ -0,0 +1,53 @@
+package com.blind.dating.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Getter
+@ToString
+@Table
+@Entity
+public class Message extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    private UserAccount sender;
+
+    @Column(nullable = false)
+    private Long receiver;
+
+    @Setter
+    @Column(nullable = false)
+    private String messageContent;
+
+    protected Message(){}
+
+    private Message(UserAccount sender, Long receiver, String messageContent) {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.messageContent = messageContent;
+    }
+
+    public static Message of(UserAccount sender, Long receiver, String messageContent){
+        return new Message(sender, receiver, messageContent);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Message)) return false;
+        Message message = (Message) o;
+        return Objects.equals(id, message.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}

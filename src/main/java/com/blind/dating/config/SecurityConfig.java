9e6b34021beab382acdfb7716d26def431a276ef
pyjhoop <pyjhoop@naver.com>
2023-07-31 10:38:34 +0900

    #6 - Spring Security Config
    SecurityConfig 파일로 접근 권한 설정을 진행함.

---
+++ src/main/java/com/blind/dating/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.blind.dating.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        return http
+                .csrf().disable()
+                .cors().and()
+                .authorizeRequests(auth->auth
+                        .antMatchers("/h2-console/**", "/swagger-ui/**").permitAll()
+                        .antMatchers("/api/login","/api/signup").permitAll()
+                        .anyRequest().authenticated())
+                .headers().frameOptions().sameOrigin()
+                .and().build();
+
+    }
+}

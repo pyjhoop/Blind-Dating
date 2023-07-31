c9497d56f49d3eea7cd35f2e8cb6308e637210cc
pyjhoop <pyjhoop@naver.com>
2023-07-31 12:41:34 +0900

    #7 - DTO 수정

--- src/main/java/com/blind/dating/dto/UserAccountDto.java
+++ src/main/java/com/blind/dating/dto/UserAccountDto.java
@@ -1,6 +1,8 @@
 package com.blind.dating.dto;
 
 import com.blind.dating.domain.UserAccount;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Setter
@@ -8,16 +10,26 @@ import lombok.*;
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(name = "UserAccountDto", description = "유저 정보 Dto")
 public class UserAccountDto {
 
+    @Schema(description = "유저 아이디")
     private String userId;
+    @Schema(description = "유저 비밀번호")
     private String userPassword;
+    @Schema(description = "닉네임")
     private String nickname;
+    @Schema(description = "사는 지역")
     private String region;
+    @Schema(description = "가중치")
     private int score;
+    @Schema(description = "MBTI")
     private String mbti;
+    @Schema(description = "성별")
     private String gender;
+    @Schema(description = "회원 탈퇴 여부")
     private Boolean deleted;
+
 
     public static UserAccountDto of(String userId, String userPassword, String nickname, String region, int score, String mbti, String gender, Boolean deleted) {
         return new UserAccountDto(userId,userPassword, nickname, region, score, mbti, gender, deleted);

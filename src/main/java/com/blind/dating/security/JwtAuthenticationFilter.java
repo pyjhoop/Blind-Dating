fd12e139a263cf5bd2e3fa84c805003f52684f8c
pyjhoop <pyjhoop@naver.com>
2023-07-31 10:42:18 +0900

    #6 - JWT 기능 구현을 위한 자바 파일 추가
    * TokenProvider를 통해 토큰 생성 메서드와 토큰 검증 메서드 구현
    * UserAccount UserDetails 구현
    * CustonUserDetailsService 작성
    * 필터링 기능을 위해 JwtAUthenticationFIlter 작성

---
+++ src/main/java/com/blind/dating/security/JwtAuthenticationFilter.java
@@ -0,0 +1,66 @@
+package com.blind.dating.security;
+
+import com.blind.dating.service.CustomUserDetailsService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final TokenProvider tokenProvider;
+    private final CustomUserDetailsService customUserDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try{
+            String token = parseBearerToken(request);
+
+            // 토큰 검증하기 JWT이므로 인가 서버에 요청하지 않아도됨
+            if(token != null || !token.equalsIgnoreCase("null")){
+                String userId = tokenProvider.validateAndGetUserId(token);
+                // SecurityContextHolder에 등록해야 인증된 사용자라고 생각한다.
+                UserDetails userDetails = customUserDetailsService.loadUserByUsername(userId);
+                AbstractAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        userDetails, // 인증된 사용자의 정보. 문자열이 아니어도 아무거나 넣을 수 있다. 보통 UserDetails라는 오브젝트 넣는다.
+                        null,
+                        userDetails.getAuthorities()
+                );
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+                securityContext.setAuthentication(authentication);
+                SecurityContextHolder.setContext(securityContext);
+            }
+        }catch (Exception e){
+            logger.error("Could not set user authentication in security context", e);
+
+        }
+
+        filterChain.doFilter(request,response);
+
+    }
+
+    private String parseBearerToken(HttpServletRequest request){
+        String bearerToken = request.getHeader("Authorization");
+
+        if(bearerToken.startsWith("Bearer")){
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+}
---
+++ src/main/java/com/blind/dating/service/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.blind.dating.service;
+
+
+import com.blind.dating.domain.UserAccount;
+import com.blind.dating.repository.UserAccountRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserAccountRepository userAccountRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        UserAccount user = userAccountRepository.findByUserId(username);
+        if(user == null){
+            throw new UsernameNotFoundException("User not found with username:"+username);
+        }
+
+        return user;
+    }
+}

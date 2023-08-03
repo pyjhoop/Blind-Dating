acfb18897a85969b35165039d8d4eb91b235eb5f
pyjhoop <pyjhoop@naver.com>
2023-08-03 19:13:17 +0900

    CORS 포트번호 수정

--- src/main/java/com/blind/dating/config/WebConfig.java
+++ src/main/java/com/blind/dating/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET","POST","DELETE","PUT")
                 .maxAge(3000);
     }

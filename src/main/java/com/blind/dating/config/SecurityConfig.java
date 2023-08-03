c9edc80d6c770b7483bdf7b41a004812984ff44d
pyjhoop <pyjhoop@naver.com>
2023-08-03 23:22:08 +0900

    #17 - 비크립트 빈등록
    추후에 편하게 사용하기 위해 빈등록한다.

--- src/main/java/com/blind/dating/config/SecurityConfig.java
+++ src/main/java/com/blind/dating/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.filter.CorsFilter;
 
@@ -29,5 +30,10 @@ public class SecurityConfig {
                         .anyRequest().authenticated())
                 .headers().frameOptions().sameOrigin()
                 .and().build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }

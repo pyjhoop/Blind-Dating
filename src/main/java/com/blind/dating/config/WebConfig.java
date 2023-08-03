48ef12ddc9aac42d93ed80944c440b230ca172a7
pyjhoop <pyjhoop@naver.com>
2023-08-03 19:28:19 +0900

    WebConfig 수정

--- src/main/java/com/blind/dating/config/WebConfig.java
+++ src/main/java/com/blind/dating/config/WebConfig.java
@@ -12,6 +12,8 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET","POST","DELETE","PUT")
+                .allowedHeaders("*")
+                .allowCredentials(true)
                 .maxAge(3000);
     }
 }

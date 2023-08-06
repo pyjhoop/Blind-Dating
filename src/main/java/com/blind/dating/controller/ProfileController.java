f6d4cf405062dbe9d2f35967abe4e3d514b61ef8
pyjhoop <pyjhoop@naver.com>
2023-08-06 13:56:01 +0900

    수정

--- src/main/java/com/blind/dating/controller/ProfileController.java
+++ src/main/java/com/blind/dating/controller/ProfileController.java
@@ -3,6 +3,7 @@ package com.blind.dating.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -10,6 +11,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/api")
 public class ProfileController {
     private final Environment env;
 

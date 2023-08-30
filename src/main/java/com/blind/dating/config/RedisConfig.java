00311fe509a4d8819e4e91048a366f9df5df8697
pyjhoop <pyjhoop@naver.com>
2023-08-30 15:53:55 +0900

    #43 실시간 채팅의 메세지브로커를 STOMP에서 Redis로 변경

--- src/main/java/com/blind/dating/config/RedisConfig.java
+++ src/main/java/com/blind/dating/config/RedisConfig.java
@@ -1,13 +1,26 @@
 package com.blind.dating.config;
 
+import com.blind.dating.service.RedisSubscriber;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+@RequiredArgsConstructor
 @Configuration
 public class RedisConfig {
+
+    private final ObjectMapper objectMapper;
 
     @Value("${spring.redis.host}")
     private String host;
@@ -16,14 +29,53 @@ public class RedisConfig {
     private int port;
 
     @Bean
+    public RedisMessageListenerContainer redisMessageListenerContainer(
+                RedisConnectionFactory connectionFactory,
+                MessageListenerAdapter listenerAdapter,
+                ChannelTopic channelTopic1,
+                ChannelTopic channelTopic2
+    ) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        //각각의 토픽에 메세지 리스너 적용시키기
+        container.addMessageListener(listenerAdapter, channelTopic1);
+        container.addMessageListener(listenerAdapter, channelTopic2);
+        return container;
+    }
+
+    @Bean
+    public MessageListenerAdapter listenerAdapter(RedisSubscriber subscriber) { // (2)
+        return new MessageListenerAdapter(subscriber, "onMessage");
+    }
+    @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
         return new LettuceConnectionFactory(host, port);
     }
 
+//    @Bean
+//    public RedisTemplate<?, ?> redisTemplate() {
+//        RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
+//        redisTemplate.setConnectionFactory(redisConnectionFactory());
+//        return redisTemplate;
+//    }
+
     @Bean
-    public RedisTemplate<?, ?> redisTemplate() {
-        RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
+    public RedisTemplate<String, Object> redisTemplate
+            (RedisConnectionFactory connectionFactory) { // (3)
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
         return redisTemplate;
     }
+
+    @Bean
+    public ChannelTopic channelTopic1() { // (4)
+        return new ChannelTopic("chatroom");
+    }
+    @Bean
+    public ChannelTopic channelTopic2() { // (4)
+        return new ChannelTopic("rooms");
+    }
+
 }
--- src/main/java/com/blind/dating/controller/StompChatController.java
+++ src/main/java/com/blind/dating/controller/StompChatController.java
@@ -3,16 +3,18 @@ package com.blind.dating.controller;
 import com.blind.dating.domain.Chat;
 import com.blind.dating.domain.ChatRoom;
 import com.blind.dating.domain.UserAccount;
-import com.blind.dating.dto.chat.ChatRequestDto;
-import com.blind.dating.dto.chat.ChatRoomDto;
+import com.blind.dating.dto.chat.*;
 import com.blind.dating.dto.response.ResponseDto;
 import com.blind.dating.handler.SessionHandler;
 import com.blind.dating.service.ChatService;
 import com.blind.dating.service.ChattingRoomService;
 import com.blind.dating.service.ReadChatService;
+import com.blind.dating.service.RedisPublisher;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -21,6 +23,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Tag(name = "Stomp Info", description = "STOMP 관련 서비스")
 @Controller
@@ -32,18 +35,14 @@ public class StompChatController {
     private final ReadChatService readChatService;
     private final ChattingRoomService chattingRoomService;
     private final SessionHandler sessionHandler;
+    private final RedisPublisher redisPublisher;
+    private final ChannelTopic channelTopic1;
+    private final ChannelTopic channelTopic2;
 
 
     //Client가 SEND할 수 있는 경로
     //stompConfig에서 설정한 applicationDestinationPrefixes와 @MessageMapping 경로가 병합됨
     //"/pub/chat/enter"
-//    @MessageMapping(value = "/chat/enter")
-//    public void enter(ChatRequestDto message, @Header("simpSessionId") String sessionId){
-//        System.out.println(sessionId+"@@@@@@@@@@@@@@@@@@@@@@@@@@@");
-//        message.setMessage(message.getWriterId() + "님이 채팅방에 참여하였습니다.");
-//        template.convertAndSend("/sub/chat/room/" + message.getChatRoomId(), message);
-//    }
-
     // roomId, userId, message,
     @MessageMapping(value = "/chat/message")
     @Operation(summary = "메세지를 전송", description = "채팅방에 접속중인 유저에게 메세지를 전송합니다.")
@@ -62,8 +61,14 @@ public class StompChatController {
         }
 
         List<ChatRoomDto> rooms = chattingRoomService.getRooms(userId);
-        template.convertAndSend("/sub/chatroom/"+userId, rooms);
-        template.convertAndSend("/sub/chat/room/" + message.getChatRoomId(), message);
+        ChatListWithUserId chatListWithUserId = new ChatListWithUserId(userId, rooms);
+        redisPublisher.publicRooms(channelTopic2, chatListWithUserId);
+        //template.convertAndSend("/sub/chatroom/"+userId, rooms);
+
+        //redis 구독
+        ChatDto dto = ChatDto.from(chat);
+        redisPublisher.publish(channelTopic1, dto);
+        //template.convertAndSend("/sub/chat/room/" + message.getChatRoomId(), message);
     }
 
     @MessageMapping(value = "/chat/disconnect")

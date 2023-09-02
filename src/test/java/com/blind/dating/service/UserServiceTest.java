b6f6598826f92cd5e067681ab84e4d60159eea2f
pyjhoop <pyjhoop@naver.com>
2023-09-02 17:46:23 +0900

    Revert "test 코드 disabled"
    This reverts commit 4e583eb9f98af2184f2a9a0178303b250fddf411.

--- src/test/java/com/blind/dating/service/UserServiceTest.java
+++ src/test/java/com/blind/dating/service/UserServiceTest.java
@@ -5,7 +5,6 @@ import com.blind.dating.dto.user.UserUpdateRequestDto;
 import com.blind.dating.repository.InterestRepository;
 import com.blind.dating.repository.UserAccountRepository;
 import com.blind.dating.repository.querydsl.UserAccountRepositoryImpl;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -26,7 +25,6 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
 
-@Disabled
 @DisplayName("UserService 테스트")
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {

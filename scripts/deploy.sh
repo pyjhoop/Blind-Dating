5f788673b053e2d4aeadb55be66104adc2e3c1e6
pyjhoop <pyjhoop@naver.com>
2023-09-05 21:25:01 +0900

    배포 테스트

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -16,8 +16,8 @@ echo ">현재 구동중인 애플리케이션 pid : $CURRENT_PID"
 if [ -z "$CURRENT_PID" ]; then
         echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-        echo "> kill -9 $CURRENT_PID"
-        kill -9 $CURRENT_PID
+        echo "> kill -15 $CURRENT_PID"
+        kill -15 $CURRENT_PID
         sleep 5
 fi
 
@@ -36,3 +36,4 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 nohup java -jar -Dspring.config.location=classpath:/application.yml,/home/ec2-user/project/application-real-db.yml,classpath:/application-real.yml -Dspring.profiles.active=real $REPOSITORY/$JAR_NAME 2>&1 &
+exec >&-

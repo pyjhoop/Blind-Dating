0e30062b769ab4992a407d8bcb61640cffd77667
pyjhoop <pyjhoop@naver.com>
2024-02-16 16:23:23 +0900

    :recycle: "JWT 토큰 예외 발생시 예외 메시지 변경을 위한 코드 수정"
    기존에는 JWT예외가 발생하면 예외 메시지가 무조건 JWT 토큰 만료로 응답이 되었는데 예외 상황에 맞게 예외 메시지를 수정

--- src/main/java/com/blind/dating/security/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/blind/dating/security/CustomAuthenticationEntryPoint.java
@@ -2,6 +2,10 @@ package com.blind.dating.security;
 
 import com.blind.dating.common.Api;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.security.SignatureException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -17,13 +21,14 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+        String message = request.getAttribute("error").toString();
 
         response.setContentType("application/json;charset=UTF-8");
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         ObjectMapper ob = new ObjectMapper();
         Api dto = Api.builder().status("unAuthorized")
                 .code(401)
-                .message("unAuthorized")
+                .message(message)
                 .build();
         response.getWriter().println(ob.writeValueAsString(dto));
 
--- src/main/java/com/blind/dating/security/JwtAuthenticationFilter.java
+++ src/main/java/com/blind/dating/security/JwtAuthenticationFilter.java
@@ -1,13 +1,14 @@
 package com.blind.dating.security;
 
-import com.blind.dating.service.CustomUserDetailsService;
-import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -15,57 +16,74 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 
 import java.io.IOException;
+import java.security.Key;
 
 @Slf4j
 @RequiredArgsConstructor
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
+    @Value("${jwt.secret_key}")
+    private String SECRET_KEY;
+
     private final TokenProvider tokenProvider;
 
-    String[] singUpRequestUrls = {"/api/signup","/api/login", "/api/check-userId","/api/check-nickname","/swagger-ui","/api/test","/docs"};
+    String[] noAuthenticationUrlList = {"/api/signup","/api/login", "/api/check-userId","/api/check-nickname","/swagger-ui","/api/test","/docs"};
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException, ServletException {
+        if(!noAuthenticate(request))
+        {
+            //엑세스 토큰
+            String token = parseBearerToken(request);
 
-        try{
-            if(!isSignUpRequest(request))
-            {
-                //엑세스 토큰
-                String token = parseBearerToken(request);
-
-                // 토큰 검증하기 JWT이므로 인가 서버에 요청하지 않아도됨
-                if(token != null && tokenProvider.validateToken(token)){
-
-                    // setContext 에 인증객체 저장하기.
-                    Authentication authentication = tokenProvider.getAuthentication(token);
-                    SecurityContextHolder.getContext().setAuthentication(authentication);
-
-                }
-            }
-
-        }catch (Exception e){
+            // 토큰 검증하기 JWT이므로 인가 서버에 요청하지 않아도됨
+            validateToken(token, request);
+            // setContext 에 인증객체 저장하기.
+            Authentication authentication = tokenProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
 
         }
-
         filterChain.doFilter(request,response);
+    }
 
+    private void validateToken(String token, HttpServletRequest request) {
+        Key key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+        } 
+        catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+            request.setAttribute("error", "잘못된 형태의 JWT입니다. 다시 요청해주세요");
+        } catch (ExpiredJwtException e) {
+
+            request.setAttribute("error","JWT 토큰이 만료되었습니다.");
+        } catch (UnsupportedJwtException e) {
+            request.setAttribute("error","지원하지 않는 토큰 형식입니다.");
+        } catch (IllegalArgumentException e) {
+            request.setAttribute("error", "JWT 토큰 안에 부적절한 인자가 섞였습니다.");
+        } catch (Exception e) {
+            request.setAttribute("error","인증에 실패했습니다.");
+        }
     }
 
     private String parseBearerToken(HttpServletRequest request){
-        String bearerToken = request.getHeader("Authorization");
-
-        if(bearerToken.startsWith("Bearer")){
-            return bearerToken.substring(7);
+        try{
+            String bearerToken = request.getHeader("Authorization");
+            if(bearerToken.startsWith("Bearer")){
+                return bearerToken.substring(7);
+            }
+        }catch (NullPointerException e){
+            request.setAttribute("error", "jwt 토큰이 존재하지 않습니다.");
         }
-        return null;
+        return "No JWT";
     }
 
-    private boolean isSignUpRequest(HttpServletRequest request) {
+    private boolean noAuthenticate(HttpServletRequest request) {
 
         String requestUri = request.getRequestURI();
 
-        for(String uri: singUpRequestUrls){
+        for(String uri: noAuthenticationUrlList){
             if(uri.contains(requestUri))
                 return true;
         }
